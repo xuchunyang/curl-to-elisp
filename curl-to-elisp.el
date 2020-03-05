dab1c6f585bd959a702084fe749964871a4447a3
Xu Chunyang <xuchunyang56@gmail.com>
2020-03-05 17:45:50 +0800

    Further parse

--- curl-to-elisp.el
+++ curl-to-elisp.el
@@ -56,5 +56,74 @@
      (eshell-parse-command command nil t))
     (user-error "Not a curl command: %S" command)))
 
+(defconst curl-to-elisp--bool-options
+  '("#" "progress-bar" "-" "next" "0" "http1.0" "http1.1" "http2"
+    "no-npn" "no-alpn" "1" "tlsv1" "2" "sslv2" "3" "sslv3" "4" "ipv4" "6" "ipv6"
+    "a" "append" "anyauth" "B" "use-ascii" "basic" "compressed" "create-dirs"
+    "crlf" "digest" "disable-eprt" "disable-epsv" "environment" "cert-status"
+    "false-start" "f" "fail" "ftp-create-dirs" "ftp-pasv" "ftp-skip-pasv-ip"
+    "ftp-pret" "ftp-ssl-ccc" "ftp-ssl-control" "g" "globoff" "G" "get"
+    "ignore-content-length" "i" "include" "I" "head" "j" "junk-session-cookies"
+    "J" "remote-header-name" "k" "insecure" "l" "list-only" "L" "location"
+    "location-trusted" "metalink" "n" "netrc" "N" "no-buffer" "netrc-file"
+    "netrc-optional" "negotiate" "no-keepalive" "no-sessionid" "ntlm" "O"
+    "remote-name" "oauth2-bearer" "p" "proxy-tunnel" "path-as-is" "post301" "post302"
+    "post303" "proxy-anyauth" "proxy-basic" "proxy-digest" "proxy-negotiate"
+    "proxy-ntlm" "q" "raw" "remote-name-all" "s" "silent" "sasl-ir" "S" "show-error"
+    "ssl" "ssl-reqd" "ssl-allow-beast" "ssl-no-revoke" "socks5-gssapi-nec" "tcp-nodelay"
+    "tlsv1.0" "tlsv1.1" "tlsv1.2" "tr-encoding" "trace-time" "v" "verbose" "xattr"
+    "h" "help" "M" "manual" "V" "version")
+  "List of curl flags that are boolean options.
+
+Adapted from URL
+`https://github.com/mholt/curl-to-go/blob/a8cb21a8885c7edc6c610d6e7db63f2891fe38d6/resources/js/curl-to-go.js#L18'.")
+
+;; curl -vH   'User-Agent: not curl' example.com
+;; curl -v -H 'User-Agent: not curl' example.com
+(defun curl-to-elisp--split (arguments)
+  "Futher parse ARGUMENTS, return a list of (OPTION . VALUE)."
+  (let ((i 0)
+        alist)
+    (while (< i (length arguments))
+      (pcase (nth i arguments)
+        ;; -- signify the end of the options
+        ("--"
+         (cl-loop for x in (nthcdr (1+ i) arguments)
+                  do
+                  (push (cons "_" x) alist)
+                  (cl-incf i)))
+        ;; long option
+        ;; --header 'Accept: application/json'
+        ;; NOTE curl does not interpret =,
+        ;; thus --header='Accept: application/json' is wrong
+        ((rx bos "--" (let name (1+ not-newline)))
+         (push (cons name
+                     (if (member name curl-to-elisp--bool-options)
+                         t
+                       (nth (cl-incf i) arguments)))
+               alist)
+         (cl-incf i))
+        ;; short option
+        ;; -abc cval
+        ;; -abccval
+        ;; NOTE curl does not interpret =, thus -abc=cval is -a -b -c =cvalue
+        ((rx bos "-" (let opts (1+ not-newline)))
+         (catch 'short
+           (cl-loop for j from 0
+                    for o in (mapcar #'string (string-to-list opts))
+                    do (if (member o curl-to-elisp--bool-options)
+                           (push (cons o t) alist)
+                         (progn
+                           (if (= j (1- (length opts)))
+                               (push (cons o (nth (cl-incf i) arguments)) alist)
+                             (push (cons o (substring opts (1+ j))) alist))
+                           (throw 'short nil)))))
+         (cl-incf i))
+        (x
+         (push (cons "_" x) alist)
+         (cl-incf i))))
+    (nreverse alist)))
+
+
 (provide 'curl-to-elisp)
 ;;; curl-to-elisp.el ends here
